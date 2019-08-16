b49cff03a05777f3fdba0d02d2a805058c938636
Brindon Sutton <brsutton22@gmail.com>
2019-08-15 20:27:59 -0500

    Add personalized greeting to header

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,29 @@
 let myImage = document.querySelector('img');
-
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/polar-bear-1.jpg') {
-        myImage.setAttribute ('src','images/polar-bear-2.jpg');
-    } else{
-        myImage.setAttribute ('src','images/polar-bear-1.jpg');
-    }
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/polar-bear-1.jpg') {
+      myImage.setAttribute ('src','images/polar-bear-2.jpg');
+  } else{
+      myImage.setAttribute ('src','images/polar-bear-1.jpg');
+  }
+} //changes the bear image when it is clicked
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Polar bears are cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Polar bears are cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
